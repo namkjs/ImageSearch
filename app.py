6c4e832b06089a217c1b7b2df5b79db6440aade1
namkjs <namlekjs@gmail.com>
2025-01-13 09:31:34 +0700

    final

--- app.py
+++ app.py
@@ -58,5 +58,4 @@ def serve_image(filename):
     return send_from_directory(folder, file)
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    app.run()
